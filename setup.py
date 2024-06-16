32b3f078d6fa11a6c73fc8e03cfce18a7b92bc19
Mihai Galos <mihai.galos@rbinternational.com>
2024-06-16 12:48:09 +0200

    feat: Release 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-breadcrumbs',
-    version='0.0.1',
+    version='0.0.2',
     description='Location-based breadcrumbs.',
     long_description='Location-based breadcrumbs.',
     keywords='mkdocs',
