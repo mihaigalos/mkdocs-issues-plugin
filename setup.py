a2d416e9e68de432f9f40710d621135b80dfe61f
Mihai Galos <mihai.galos@rbinternational.com>
2024-06-16 12:54:13 +0200

    feat: Release 0.0.3

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='mkdocs-breadcrumbs',
-    version='0.0.2',
+    version='0.0.3',
     description='Location-based breadcrumbs.',
-    long_description='Location-based breadcrumbs.',
-    keywords='mkdocs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='mkdocs plugin',
     url='https://github.com/mihaigalos/mkdocs-breadcrumbs',
     author='Mihai Galos',
     author_email='mihai@galos.one',
     license='MIT',
-    python_requires='>=3',
+    python_requires='>=3.8',
     install_requires=[
         'mkdocs>=1.0.4',
         'mkdocs-material'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(),
     entry_points={
