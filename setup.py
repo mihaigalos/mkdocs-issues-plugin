1e7b1f8510f2eb89ff40ec0f75df7444151f864e
Mihai Galos <mihai.galos@rbinternational.com>
2024-07-18 22:05:22 +0200

    feat: Release 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mkdocs-issues-plugin',
-    version='0.0.5',
+    version='0.0.6',
     description='Issues status and labels in mkdocs.',
     long_description=long_description,
     long_description_content_type="text/markdown",
