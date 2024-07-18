5944bfa17a8014c442473ff3918df706a2ccd0a1
Mihai Galos <mihai.galos@rbinternational.com>
2024-07-18 19:22:07 +0200

    chore: Release 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mkdocs-issues-plugin',
-    version='0.0.2',
+    version='0.0.3',
     description='Issues status and labels in mkdocs.',
     long_description=long_description,
     long_description_content_type="text/markdown",
