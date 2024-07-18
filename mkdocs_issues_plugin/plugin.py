f8df06e2e2c8254a25ad180ce5ddff6ef8068f31
Mihai Galos <mihai.galos@rbinternational.com>
2024-07-18 12:49:07 +0200

    feat: GitLab - Initial support

--- mkdocs_issues_plugin/plugin.py
+++ mkdocs_issues_plugin/plugin.py
@@ -30,15 +30,24 @@ class Issues(BasePlugin):
     def on_page_markdown(self, markdown, **kwargs):
         logger.debug("Processing page markdown")
         for conf in self.config['configs']:
+            service = conf['service']
             base_url = conf['base_url']
             api_url = conf['api_url']
             token = conf['token']
             headers = {'Authorization': f'token {token}'} if token else {}
-            issue_pattern = re.compile(rf'{re.escape(base_url)}/([^/]+)/([^/]+)/issues/(\d+)')
+            if service == 'gitlab':
+                headers = {'Private-Token': token} if token else {}
+
+            issue_pattern = re.compile(rf'{re.escape(base_url)}/([^/]+)/([^/]+)/issues/(\d+)' if service == 'github' else rf'{re.escape(base_url)}/([^/]+)/([^/]+)/-/issues/(\d+)')
             logger.debug(f"Regex pattern for {base_url}: {issue_pattern.pattern}")
 
             def fetch_issue_status(owner, repo, issue_number):
-                url = f"{api_url}/repos/{owner}/{repo}/issues/{issue_number}"
+                if service == 'github':
+                    url = f"{api_url}/repos/{owner}/{repo}/issues/{issue_number}"
+                elif service == 'gitlab':
+                    repo_encoded = f"{owner}%2F{repo}"
+                    url = f"{api_url}/projects/{repo_encoded}/issues/{issue_number}"
+
                 logger.debug(f"Fetching issue from URL: {url}")
                 try:
                     response = requests.get(url, headers=headers)
@@ -50,7 +59,7 @@ class Issues(BasePlugin):
                 issue = response.json()
                 status = issue.get('state', 'unknown')
                 labels = [
-                    {'name': label['name'], 'color': label['color']}
+                    {'name': label['name'], 'color': label['color'] if service == 'github' else '007BFF'}
                     for label in issue.get('labels', [])
                 ]
                 return status, labels
@@ -70,7 +79,11 @@ class Issues(BasePlugin):
                     for label in labels
                 ) or 'No labels'
 
-                issue_info = f'<span title="{status_title}">{status_icon}</span> [{owner}/{repo}#{issue_number}]({match.group(0)}) {labels_str}'
+                if service == 'github':
+                    issue_info = f'<span title="{status_title}">{status_icon}</span> [{owner}/{repo}#{issue_number}]({match.group(0)}) {labels_str}'
+                elif service == 'gitlab':
+                    issue_info = f'<span title="{status_title}">{status_icon}</span> [{owner}/{repo}#{issue_number}]({match.group(0).replace("/-/issues/", "/issues/")}) {labels_str}'
+
                 markdown = markdown.replace(match.group(0), issue_info)
                 logger.debug(f"Processed issue: {owner}/{repo}#{issue_number} with status: {status}")
 
